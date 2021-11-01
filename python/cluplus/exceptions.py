bfbbdcd058db4baad7ffcb062370c6fefe96c5c0
Florian Briegel <wasndas@zone42.de>
2021-11-01 23:43:31 +0100

    added exceptions

---
+++ python/cluplus/exceptions.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+#
+# @Author: Florian Briegel (briegel@mpia.de)
+# @Date: 2021-08-18
+# @Filename: exceptions.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+
+class ProxyException(Exception):
+    """Base proxy exception"""
+    
+    pass
+
+
+class ProxyPartialInvokeException(ProxyException):
+    """One or more invocation failed"""
+
+    pass
