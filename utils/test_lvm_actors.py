958dc354eec539a152f39b4a117073864c89f64a
Florian Briegel <wasndas@zone42.de>
2021-11-24 13:40:34 +0100

    0.0.5

---
+++ utils/test_lvm_actors.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+#
+# @Author: Florian Briegel (briegel@mpia.de)
+# @Date: 2021-08-18
+# @Filename: test_lvm_actor.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+
+class LVM():
+    class SCI():
+        FOC = "lvm.sci.foc"
+        KM = "lvm.sci.km"
+        PWI = "lvm.sci.pwi"
+    SCI = SCI()
+
+    class SKYE():
+        FOC = "lvm.skye.foc"
+        KM = "lvm.skye.km"
+        PWI = "lvm.skye.pwi"
+    SKYE = SKYE()
+
+    class SKYW():
+        FOC = "lvm.skyw.foc"
+        KM = "lvm.skyw.km"
+        PWI = "lvm.skyw.pwi"
+    SKYW = SKYW()
+
+    class SPEC():
+        FIBSEL = "lvm.spec.fibsel"
+        FOC = "lvm.spec.foc"
+        PWI = "lvm.spec.pwi"
+    SPEC = SPEC()
