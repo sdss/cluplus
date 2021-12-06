65ed2806e18a8ab7133b6a5c4d98aef85048619d
Florian Briegel <wasndas@zone42.de>
2021-12-06 16:51:25 +0100

    added depricated first proxy

--- tests/proto/actor/commands/complex_data_with_jsonstring.py
+++ tests/proto/actor/commands/complex_data_with_jsonstring.py
@@ -45,29 +45,29 @@ async def sendData(command: Command, listdata, dictdata, mixeddata):
     command.finish()
 
 
-fits_dict = { \
-'WCSAXES': [                    2, "number of World Coordinate System axes"], \
-'CRPIX1': [               535.384, "x-coordinate of reference pixel"], \
-'CRPIX2': [                536.67, "y-coordinate of reference pixel"], \
-'CRVAL1': [    8.561000000000E+03, "first axis value at reference pixel"], \
-'CRVAL2': [    0.000000000000E+00, "second axis value at reference pixel2"], \
-'CTYPE1': [  'LAMBDA  '          , "the coordinate type for the first axis"], \
-'CTYPE2': [  'ANGLE   '          , "the coordinate type for the second axis"], \
-'CD1_1 ': [                 0.554, "partial of first axis coordinate w.r.t. x"], \
-'CD1_2 ': [                    0., "partial of first axis coordinate w.r.t. y"], \
-'CD2_1 ': [                    0., "partial of second axis coordinate w.r.t. x"], \
-'CD2_2 ': [   1.38889000000000E-5, "partial of second axis coordinate w.r.t. y"], \
-'LTV1  ': [                   19., "offset in X to subsection start"], \
-'LTV2  ': [                   20., "offset in Y to subsection start"], \
-'LTM1_1': [                    1., "reciprocal of sampling rate in X"], \
-'LTM2_2': [                    1., "reciprocal of sampling rate in Y"], \
-'RA_APER': [   1.761216666667E+02, "RA of aperture reference position"], \
-'DEC_APER': [   4.851611111111E+01, "Declination of aperture reference position"], \
-'PA_APER': [   1.143617019653E+02, "Position Angle of reference aperture center [de"], \
-'DISPAXIS': [                    1, "dispersion axis; 1 = axis 1, 2 = axis 2, none"], \
-'CUNIT1': [  'angstrom'          , "units of first coordinate value"], \
-'CUNIT2': [  'deg     '          , "units of second coordinate value"] \
-}
+fits_dict =  [ \
+['WCSAXES',                    2, "number of World Coordinate System axes"], \
+['CRPIX1',               535.384, "x-coordinate of reference pixel"], \
+['CRPIX2',                536.67, "y-coordinate of reference pixel"], \
+['CRVAL1',    8.561000000000E+03, "first axis value at reference pixel"], \
+['CRVAL2',    0.000000000000E+00, "second axis value at reference pixel2"], \
+['CTYPE1',  'LAMBDA  '          , "the coordinate type for the first axis"], \
+['CTYPE2',  'ANGLE   '          , "the coordinate type for the second axis"], \
+['CD1_1 ',                 0.554, "partial of first axis coordinate w.r.t. x"], \
+['CD1_2 ',                    0., "partial of first axis coordinate w.r.t. y"], \
+['CD2_1 ',                    0., "partial of second axis coordinate w.r.t. x"], \
+['CD2_2 ',   1.38889000000000E-5, "partial of second axis coordinate w.r.t. y"], \
+['LTV1  ',                   19., "offset in X to subsection start"], \
+['LTV2  ',                   20., "offset in Y to subsection start"], \
+['LTM1_1',                    1., "reciprocal of sampling rate in X"], \
+['LTM2_2',                    1., "reciprocal of sampling rate in Y"], \
+['RA_APER',   1.761216666667E+02, "RA of aperture reference position"], \
+['DEC_APER',   4.851611111111E+01, "Declination of aperture reference position"], \
+['PA_APER',   1.143617019653E+02, "Position Angle of reference aperture center [de"], \
+['DISPAXIS',                    1, "dispersion axis; 1 = axis 1, 2 = axis 2, none"], \
+['CUNIT1',  'angstrom'          , "units of first coordinate value"], \
+['CUNIT2',  'deg     '          , "units of second coordinate value"] \
+]
 
 
 @command_parser.command("fitsStyleData")
