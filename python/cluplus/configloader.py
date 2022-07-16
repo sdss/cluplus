36e9b73cd762fa1e6c827b9005762b0c8f2ececd
Florian Briegel <wasndas@zone42.de>
2022-07-16 10:29:09 -0700

    added enviroment expansion to config loader

--- python/cluplus/configloader.py
+++ python/cluplus/configloader.py
@@ -9,15 +9,19 @@
 import os
 
 from yaml import SafeLoader, load
-
+from expandvars import expand
 
 class Loader(SafeLoader):
     def __init__(self, stream):
         self._root = os.path.split(stream.name)[0]
         super(Loader, self).__init__(stream)
         self.add_constructor('!include', Loader.include)
+        self.add_constructor('!env', Loader.env)
 
     def include(self, node):
         filename = os.path.join(self._root, self.construct_scalar(node))
         with open(filename, 'r') as f:
             return load(f, Loader)
+
+    def env(self, node):
+        return expand(self.construct_scalar(node))
--- tests/test_01_configloader.py
+++ tests/test_01_configloader.py
@@ -11,22 +11,35 @@ import pytest
 
 # from clu import JSONActor
 from clu import AMQPActor
-
 from cluplus.configloader import Loader
 
 
-@pytest.fixture
-def lvmnps_config():
-    config_file = os.path.join(os.path.dirname(__file__),
+@pytest.fixture(scope="session")
+def lvmnps_config_file():
+    return os.path.join(os.path.dirname(__file__),
                                "proto/etc/lvmnps/lvmnps_dli.yml")
-    actor = AMQPActor.from_config(config_file, loader=Loader)
-    return actor.config
 
 
 @pytest.mark.asyncio
-async def test_config(lvmnps_config):
-    assert "switches" in lvmnps_config
-    assert(lvmnps_config["switches"]["DLI_NPS_01"]["hostname"]
+async def test_config(lvmnps_config_file):
+
+    actor = AMQPActor.from_config(lvmnps_config_file, loader=Loader)
+
+    assert "switches" in actor.config
+    assert(actor.config["switches"]["DLI_NPS_01"]["hostname"]
            == "10.7.45.22")
-    assert(lvmnps_config["switches"]["DLI_NPS_02"]["password"]
+    assert(actor.config["switches"]["DLI_NPS_02"]["password"]
            == "VCrht9wfx2CQN9b")
+
+    assert(actor.config["actor"]["host"]
+           == "localhost")
+
+@pytest.mark.asyncio
+async def test_config_environ(lvmnps_config_file):
+
+    os.environ["SHOULD_NOT_EXIST_SOMWHERE"]="somewhere"
+    
+    actor = AMQPActor.from_config(lvmnps_config_file, loader=Loader)
+
+    assert(actor.config["actor"]["host"]
+           == "somewhere")
